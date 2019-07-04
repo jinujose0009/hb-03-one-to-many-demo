018251c8e4500720d0afc16fc4eb7542fe51e154
cluebox <jinucluebox@gmail.com>
2019-07-04 10:53:57 +0530

    First commit

---
+++ hb-03-one-to-many-demo/src/com/jinu/entity/Course.java
@@ -0,0 +1,60 @@
+package com.jinu.entity;
+
+import javax.persistence.CascadeType;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import javax.persistence.OneToMany;
+import javax.persistence.Table;
+
+@Entity
+@Table(name="course")
+public class Course {
+	
+	@Id
+	@GeneratedValue(strategy=GenerationType.IDENTITY)
+	@Column(name="id")
+	private int id;
+	
+	@Column(name="title")
+	private String title;
+	
+	@ManyToOne(cascade={CascadeType.DETACH,CascadeType.MERGE,CascadeType.PERSIST,CascadeType.REFRESH})
+	@JoinColumn(name="instructor_id")
+	private Instructor instroctor;
+	public Course() {
+	}
+	public Course(String title) {
+		this.title = title;
+	}
+	public int getId() {
+		return id;
+	}
+	public void setId(int id) {
+		this.id = id;
+	}
+	public String getTitle() {
+		return title;
+	}
+	public void setTitle(String title) {
+		this.title = title;
+	}
+	public Instructor getInstroctor() {
+		return instroctor;
+	}
+	public void setInstroctor(Instructor instroctor) {
+		this.instroctor = instroctor;
+	}
+	@Override
+	public String toString() {
+		return "Cource [id=" + id + ", title=" + title + ", instroctor="
+				+ instroctor + "]";
+	}
+	
+	
+
+}
